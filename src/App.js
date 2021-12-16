b1c8a306142845638bfc97bdf8427651b0cafb41
Stephanie <stephaniereyes@pursuit.org>
2021-12-16 10:48:55 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { Component } from "react";
 import "./App.css";
+import {Switch, Route} from "react-router-dom";
+import  Home from "./components/Home"
+import Movies from "./components/Movies"
+import Locations from "./components/Locations"
+import People from "./components/People";
 
-function App() {
+class App extends Component{
+  render(){
   return (
     <div className="app">
-      <main>
-        <h1>Hello, world!</h1>
-      </main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/locations" component={Locations} />
+        <Route path="/people" component={People} />
+      </Switch>
     </div>
   );
+}
 }
 
 export default App;
