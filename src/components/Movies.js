711284445ee4cbfc611f915d9ad279a2c33e3014
Stephanie <stephaniereyes@pursuit.org>
2021-12-16 11:53:13 -0500

    more progress

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,15 +4,44 @@ class Movies extends Component {
     constructor(){
         super()
         this.state ={
+            movies:[],
+            movieselected:null
 
         }
     }
 
+    fetchMovies = () => {
+        fetch("https://ghibliapi.herokuapp.com/films")
+        .then((res)=>res.json())
+        .then((data)=>{
+            this.setState({
+                movies: data,
+            });
+        });
+    };
+
+    componentDidMount = () => {
+      this.fetchMovies()
+    }
+    handleDropDown = (event) =>{
+        this.setState({
+            movieselected:event.target.value
+        })
+    }
+
+    
     render() {
+        let dropDown = this.state.movies.map((movie)=>{
+            return <option>{movie.title}</option>;
+        })
       return <div className="Movies">
       
       <h1>Select a Movie</h1>
-      
+
+      <select>
+          <option></option>
+          {dropDown}
+      </select>
       </div>
     }
   }
