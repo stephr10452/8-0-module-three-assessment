abe5eaa6132710c3ec7213db642a92c799e49122
Stephanie <stephaniereyes@pursuit.org>
2021-12-16 17:01:21 -0500

    more changes

---
+++ src/components/Locations.js
@@ -0,0 +1,13 @@
+import {Component} from "react";
+import "../App.css"
+class Locations extends Component {
+    render() {
+      return <locations>
+      
+      <h1>List of Locations</h1>
+      
+      </locations>
+    }
+  }
+  
+  export default Locations;
--- src/components/People.js
+++ src/components/People.js
@@ -13,38 +13,38 @@ class People extends Component {
         .then((res)=>res.json())
         .then((data)=>{
             this.setState({
-                people: data,
+                people: data
+                
             });
         });
     };
-    handleSubmit = (event) =>{
-        event.preventDefault();
-    }
+   
     handleInput=(event)=>{
         this.setState({
             [event.target.name]: event.target.value
         })
     }
 
-    componentDidMount = () =>{
+    handleSubmit = (event) =>{
+        event.preventDefault()
         this.fetchPeople()
+        
     }
      
     
     render() {
-        let chosenPerson = this.state.people.map((person)=>{
-         return person.name
-        })
+        
       return <people>
       
       <h1>Search for a Person</h1>
            
         <form onSubmit={this.handleSubmit}>
-            <input  onInput={this.handleInput}
-            name="comment" 
-            id="comment"
+            <input 
+             onInput={this.handleInput}
+            name="name" 
+            id="name"
             placeholder="Find Your Person"
-            value={chosenPerson} 
+            
                 />
             
             <button id="button" type="Submit" >Submit</button>   
